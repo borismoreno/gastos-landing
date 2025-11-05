29b8bc2ae7c621dfb5ad94d07e5e30c989237ff4
Boris Moreno <boris_marco_moreno@hotmail.com>
2025-11-05 17:10:54 -0500

    server islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://polite-salmiakki-0d0649.netlify.app/'
+  site: 'https://polite-salmiakki-0d0649.netlify.app/',
+  integrations: [preact()]
 });
